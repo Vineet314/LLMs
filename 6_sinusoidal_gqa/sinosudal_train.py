7e5a449a9c457ad87fb4edc7a7bffd59ca55ac8d
Vineet <telangvineet@gmail.com>
2025-07-13 23:11:06 +0530

    configs

--- 6_sinusoidal_gqa/sinosudal_train.py
+++ 6_sinusoidal_gqa/sinosudal_train.py
@@ -17,34 +17,59 @@ import math
 from dataclasses import dataclass
 from time import time
 
-from sinosudal_llm import LLM
+from sinusoidal_llm import LLM
 
 torch.set_float32_matmul_precision("high") # OPTIM 1 brought dt from 230 to 170
 
 @dataclass
-class config:
+class LLMconfig:
     # hyperparameters
-    batch_size = 4 # how many independent sequences will we process in parallel?
-    block_size = 1024 # what is the maximum context length for predictions?
-    vocab_size = 50304 # OPTIM 4 (along with grad clipping) brought dt from 95 to 90
+    block_size : int  # what is the maximum context length for predictions?
+    vocab_size : int # OPTIM 4 (along with grad clipping) brought dt from 95 to 90
+    n_kv_heads : int
+    n_embd : int
+    n_head : int
+    n_layer: int
+    dropout: float
 
-    max_iters = 500
-    eval_interval = 50
-    learning_rate = 3e-4
-    warmup_steps = 25
-    max_decay_steps = 75
+@dataclass
+class Trainconfig:
+    total_batch_size : int
+    batch_size : int
+    max_iters : int
+    eval : bool
+    eval_interval : int
+    learning_rate : float
+    warmup_steps : int
+    max_decay_steps : int
+    device : str
+    eval_iters : int
+    compile : bool #= False if os.name != 'posix' else True
+    save_model : bool
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    eval_iters = 200
-    compile = False if os.name != 'posix' else True
-    save_model = True
+ModelConfig = LLMconfig(
+    # hyperparameters
+    block_size = 1024, # what is the maximum context length for predictions?
+    vocab_size = 50304, # OPTIM 4 (along with grad clipping) brought dt from 95 to 90
+    n_kv_heads = 4,
+    n_embd = 768,
+    n_head = 8,
+    n_layer= 6,
+    dropout= 0.2)
 
-    n_embd = 384
-    n_head = 6
-    n_layer = 6
-    n_kv_heads = 2 # Set to 6 for MHA, 1 for MQA, or another divisor of n_head for GQA
-    dropout = 0.2
-    total_batch_size = 2**16
+TrainingConfig = Trainconfig(
+    total_batch_size = 2**16,
+    batch_size = 4, # how many independent sequences will we process in parallel?
+    max_iters = 500,
+    eval = False,
+    eval_interval = 50,
+    learning_rate = 3e-4,
+    warmup_steps = 25,
+    max_decay_steps = 75,
+    device = 'cuda' if torch.cuda.is_available() else 'cpu',
+    eval_iters = 200,
+    compile = False if os.name != 'posix' else True,
+    save_model = True)
 
 class DataLoader:
     def __init__(self, B, T):
@@ -71,29 +96,29 @@ class DataLoader:
             self.current_position = 0
         return x,y
 
-def get_lr(iter, config):
-    max_lr = config.learning_rate
+def get_lr(iter, TrainingConfig:Trainconfig):
+    max_lr = TrainingConfig.learning_rate
     min_lr = max_lr*0.1
     # 1) linear warump for warmup_steps:
-    if iter < config.warmup_steps:
-        return max_lr * (iter+1)/config.warmup_steps
+    if iter < TrainingConfig.warmup_steps:
+        return max_lr * (iter+1)/TrainingConfig.warmup_steps
     #2) if iter > lr_decay_iters, return min_lr
-    elif iter > config.max_decay_steps:
+    elif iter > TrainingConfig.max_decay_steps:
         return min_lr
     #3) in between, use cosine decay
     else:
-        decay_ratio = (iter - config.warmup_steps) / (config.max_decay_steps - config.warmup_steps)
+        decay_ratio = (iter - TrainingConfig.warmup_steps) / (TrainingConfig.max_decay_steps - TrainingConfig.warmup_steps)
         decay_ratio = min(decay_ratio, 1.0)  # ensure it does
         coeff = 0.5 * (1 + math.cos(math.pi * decay_ratio))
         return min_lr + coeff * (max_lr - min_lr)
 
 @torch.no_grad()
-def estimate_loss(model:LLM, config:config, eval_loader:DataLoader):
+def estimate_loss(model:LLM, TrainingConfig:Trainconfig, eval_loader:DataLoader):
     out = {}
     model.eval()
     for split in ['train', 'val']:
-        losses = torch.zeros(config.eval_iters)
-        for k in range(config.eval_iters):
+        losses = torch.zeros(TrainingConfig.eval_iters)
+        for k in range(TrainingConfig.eval_iters):
             X, Y = eval_loader.next_batch()
             _, loss, _ = model(X, Y)
             losses[k] = loss.item()
@@ -103,42 +128,43 @@ def estimate_loss(model:LLM, config:config, eval_loader:DataLoader):
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a simple LLM model')
-    parser.add_argument('--batch_size',    type=int,   default=config.batch_size,    help='Batch size for training')
-    parser.add_argument('--block_size',    type=int,   default=config.block_size,    help='Block size for training')
-    parser.add_argument('--max_iters',     type=int,   default=config.max_iters,     help='Maximum number of iterations for training')
-    parser.add_argument('--eval_interval', type=int,   default=config.eval_interval, help='Interval for evaluation')
-    parser.add_argument('--learning_rate', type=float, default=config.learning_rate, help='Learning rate for training')
-    parser.add_argument('--device',        type=str,   default=config.device,        help='Device to use for training (cpu or cuda)')
-    parser.add_argument('--eval_iters',    type=int,   default=config.eval_iters,    help='Number of iterations for evaluation')
-    parser.add_argument('--n_embd',        type=int,   default=config.n_embd,        help='Number of embedding dimensions')
-    parser.add_argument('--n_head',        type=int,   default=config.n_head,        help='Number of attention heads')
-    parser.add_argument('--n_kv_heads',    type=int,   default=config.n_kv_heads,    help='Number of key/value heads for GQA')
-    parser.add_argument('--n_layer',       type=int,   default=config.n_layer,       help='Number of layers in the model')
-    parser.add_argument('--dropout',       type=float, default=config.dropout,       help='Dropout rate')
-    parser.add_argument('--vocab_size',    type=int,   default=config.vocab_size,    help='Vocabulary size for the model')
-    parser.add_argument('--warmup_steps',  type=int,   default=config.warmup_steps,  help='Number of warmup steps for learning rate')
-    parser.add_argument('--max_decay_steps',type=int, default=config.max_decay_steps,help='Maximum decay steps for learning rate')
-    parser.add_argument('--total_batch_size_str', type=str,      default='2**16',    help='Total batch size for training passed in as a string expression')
+    parser.add_argument('--batch_size',    type=int,   default=TrainingConfig.batch_size,    help='Batch size for training')
+    parser.add_argument('--max_iters',     type=int,   default=TrainingConfig.max_iters,     help='Maximum number of iterations for training')
+    parser.add_argument('--eval_interval', type=int,   default=TrainingConfig.eval_interval, help='Interval for evaluation')
+    parser.add_argument('--learning_rate', type=float, default=TrainingConfig.learning_rate, help='Learning rate for training')
+    parser.add_argument('--device',        type=str,   default=TrainingConfig.device,        help='Device to use for training (cpu or cuda)')
+    parser.add_argument('--eval_iters',    type=int,   default=TrainingConfig.eval_iters,    help='Number of iterations for evaluation')
+    parser.add_argument('--warmup_steps',  type=int,   default=TrainingConfig.warmup_steps,  help='Number of warmup steps for learning rate')
+    parser.add_argument('--max_decay_steps',type=int,  default=TrainingConfig.max_decay_steps,help='Maximum decay steps for learning rate')
+
+    parser.add_argument('--n_embd',        type=int,   default=ModelConfig.n_embd,          help='Number of embedding dimensions')
+    parser.add_argument('--n_head',        type=int,   default=ModelConfig.n_head,          help='Number of attention heads')
+    parser.add_argument('--n_kv_heads',    type=int,   default=ModelConfig.n_kv_heads,      help='Number of key/value heads for GQA')
+    parser.add_argument('--n_layer',       type=int,   default=ModelConfig.n_layer,         help='Number of layers in the model')
+    parser.add_argument('--dropout',       type=float, default=ModelConfig.dropout,         help='Dropout rate')
+    parser.add_argument('--vocab_size',    type=int,   default=ModelConfig.vocab_size,      help='Vocabulary size for the model')
+    parser.add_argument('--block_size',    type=int,   default=ModelConfig.block_size,      help='Block size for training')
+    parser.add_argument('--total_batch_size_str', type=str,      default='2**16',         help='Total batch size for training passed in as a string expression')
     parser.add_argument('--compile', action='store_true', help='Whether to compile the model with torch.compile()')
     parser.add_argument('--save_model', action='store_true', help='Whether to save the model after training')
     return parser.parse_args()
 
-def main(model:LLM, config:config, optimizer:torch.optim.Optimizer):
-    B = config.batch_size
-    T = config.block_size
-    assert config.total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T"
-    grad_accum_steps = config.total_batch_size // (B * T)
+def main(model:LLM, TrainingConfig:Trainconfig, ModelConfig:LLMconfig, optimizer:torch.optim.Optimizer):
+    B = TrainingConfig.batch_size
+    T = ModelConfig.block_size
+    assert TrainingConfig.total_batch_size % (B * T) == 0, "make sure total_batch_size is divisible by B * T"
+    grad_accum_steps = TrainingConfig.total_batch_size // (B * T)
 
-    train_loader = DataLoader(B=config.batch_size, T=config.block_size)
-    eval_loader  = DataLoader(B=config.batch_size, T=config.block_size)
+    train_loader = DataLoader(B=TrainingConfig.batch_size, T=ModelConfig.block_size)
+    eval_loader  = DataLoader(B=TrainingConfig.batch_size, T=ModelConfig.block_size)
 
-    for iter in range(config.max_iters):
+    for iter in range(TrainingConfig.max_iters):
         t3 = t4 = 0
         t0 = time() 
         # every once in a while evaluate the loss on train and val sets
-        # if iter % config.eval_interval == 0 or iter == config.max_iters - 1:
+        # if iter % ModelConfig.eval_interval == 0 or iter == ModelConfig.max_iters - 1:
             # t3 = time()
-        #     losses = estimate_loss(model, config, eval_loader)
+        #     losses = estimate_loss(model, ModelConfig, eval_loader)
             # print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
             # t4 = time()
         optimizer.zero_grad(set_to_none=True)
@@ -146,10 +172,10 @@ def main(model:LLM, config:config, optimizer:torch.optim.Optimizer):
 
         for micro_step in range(grad_accum_steps):
             x, y = train_loader.next_batch()
-            x,y = x.to(device=config.device), y.to(device=config.device)
+            x,y = x.to(device=TrainingConfig.device), y.to(device=TrainingConfig.device)
             # evaluate the loss
             if torch.cuda.is_bf16_supported(): # OPTIM 2 brought dt from 170 to 130
-                with torch.autocast(device_type=config.device, dtype=torch.bfloat16):
+                with torch.autocast(device_type=TrainingConfig.device, dtype=torch.bfloat16):
                     _, loss, _ = model(x,y)
             else: # need to learn about gradient scalers 
                 _, loss, _ = model(x,y)
@@ -158,7 +184,7 @@ def main(model:LLM, config:config, optimizer:torch.optim.Optimizer):
             loss.backward()
 
         norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-        lr = get_lr(iter, config) # OPTIM 5 : i plugged in,now its almost 68ms
+        lr = get_lr(iter, Trainconfig) # OPTIM 5 : i plugged in,now its almost 68ms
         for param_grp in optimizer.param_groups:
             param_grp['lr'] = lr
         optimizer.step()
@@ -167,7 +193,7 @@ def main(model:LLM, config:config, optimizer:torch.optim.Optimizer):
         dt = (t1-t0-(t4-t3))*1000
         print(f"step: {iter} | train loss:{loss_accum:.4f} | dt: {dt:.2f}ms | grad_acum_steps:{grad_accum_steps}")
 
-    if config.save_model:
+    if TrainingConfig.save_model:
         torch.save(model, 'mqa_gqa_llm_model.pt')
         print("\nsaved run to mqa_gqa_llm_model.pt")
 
@@ -177,19 +203,22 @@ if __name__ == '__main__':
         # need to eval the total_batch_size to get the grad_accum_steps
         if key == 'total_batch_size_str':
             value = eval(value)
-            setattr(config, 'total_batch_size', value)
+            setattr(TrainingConfig, 'total_batch_size', value)
         else:
-            setattr(config, key, value)
+            if hasattr(Trainconfig, key):
+                setattr(TrainingConfig, key, value)
+            else:
+                setattr(ModelConfig, key, value)
 
-    model = LLM(config).to(config.device)
-    if config.compile: # OPTIM 3 brought dt from 130 to 95ms
+    model = LLM(ModelConfig).to(TrainingConfig.device)
+    if TrainingConfig.compile: # OPTIM 3 brought dt from 130 to 95ms
         print("Compiling the model with torch.compile()")
         model = torch.compile(model)
 
     # Training
     print(f"total parameters = {model.get_num_params():,}")
     # OPTIM 6: dt 68ms to 60ms
-    optimizer = model.configure_optimizers(weight_decay=0.1,learning_rate=config.learning_rate,device=config.device,prints=False)
+    optimizer = model.configure_optimizers(weight_decay=0.1,learning_rate=TrainingConfig.learning_rate,device=TrainingConfig.device,prints=False)
 
     # start training
-    main(model, config, optimizer)
+    main(model=model, TrainingConfig=TrainingConfig, ModelConfig=ModelConfig, optimizer=optimizer)
