c210a49c6282185fb60cb29577e76b59c9af90c1
Vineet <telangvineet@gmail.com>
2025-07-19 01:59:57 +0530

    add mega script

---
+++ build_llm.py
@@ -0,0 +1,21 @@
+'''This script builds an LLM model based on the user's CLI inputs. Available settings to choose from : 
+1. Attention Type (with or without KV caching): 
+    - Multi Head Attention
+    - Flash Attention based on Multi Head Attention (MHA)
+    - Flash Attention based on Multi Query Attention (MQA)
+    - Flash Attention based on Grouped Query Attention (GQA)
+    - Multi Head Latent Attention (MHLA)
+    - (Work in progress) Flash Multi Head Latent Attention
+
+2. Positional Encodings:
+    - Learnable PE
+    - Sinusoidal PE
+    - Rotary PE (RoPE)
+'''
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+import argparse
